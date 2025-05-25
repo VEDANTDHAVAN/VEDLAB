dfe4b9f5bbc1f2f15b09c620d9b27261bfb3bb55
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2025-05-25 16:57:20 +0530

    created signup with posgreSql connection

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+import { redirect } from "next/navigation";
+import { ZodError } from "zod";
+import { signUpSchema } from "~/schemas";
+import { db } from "~/server/db";
+
+export async function register(
+    prevState: string | undefined, 
+    formData: FormData
+) {
+ try {
+    const {email, password} = await signUpSchema.parseAsync({
+        email: formData.get("email"),
+        password: formData.get("password")
+    });
+
+    const user = await db.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+
+    if(user) {
+        return "User Already Exists!!"
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+        data: {
+            email: email,
+            password: hash,
+        },
+    });
+
+ } catch (error) {
+    if(error instanceof ZodError) {
+        return error.errors.map((error) => error.message).join(", ")
+    }
+ }
+
+ redirect("/signin")
+}
